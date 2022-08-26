ca73bdf5873b2f9c946c13d85dcc4d70aa7cf837
Mihai Popeanga <doru@DESKTOP-92BSIG0.localdomain>
2022-08-26 14:51:13 +0200

    update installs

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install -y code zsh tmux nginx maim rg xclip
-sudo apt-get install ripgrep
+sudo apt install zsh tmux nginx maim ripgrep xclip -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
@@ -10,6 +9,6 @@ curl -sL https://j.mp/glab-cli | sudo sh
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-# plugins=(z zsh-autosuggestions zsh-syntax-highlighting fzf-zsh-plugin)
+# plugins=(z zsh-autosuggestions zsh-syntax-highlighting)
 # to set vscode as default editor
 git config --global core.editor "code --wait"
